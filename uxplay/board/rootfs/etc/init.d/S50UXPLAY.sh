ef1d80b3aed14e035213d9c54fd604c01b87885b
Miguel Fuertes <hkfuertes@gmail.com>
2024-02-20 14:47:14 +0100

    Update S50UXPLAY.sh

--- uxplay/board/rootfs/etc/init.d/S50UXPLAY.sh
+++ uxplay/board/rootfs/etc/init.d/S50UXPLAY.sh
@@ -6,8 +6,6 @@ then
     exit 2
 fi
 
-systemctl stop getty@tty1
-
 # Set Variables
 export PIN=$(printf %04d ${RANDOM:0:4})
 export NAME=$(hostname | tr '[:lower:]' '[:upper:]')
@@ -15,7 +13,7 @@ export SIZE=$(cat /sys/class/graphics/fb0/virtual_size | sed -e 's/,/x/g')
 
 # Display $PIN with fbi
 convert -size $SIZE xc:black -gravity southeast -pointsize 48 -draw "fill white text 50,20 '$PIN'" /tmp/default.jpg
-fbi -T 1 -noedit -nocomments -a -noverbose /tmp/default.jpg
+fbv -T 1 -noedit -nocomments -a -noverbose /tmp/default.jpg
 
 # Start UxPlay
 uxplay -n $NAME -nh -pin $PIN
