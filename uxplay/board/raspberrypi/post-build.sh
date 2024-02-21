0bd0e4b4fd953ad52eba753465c0c2f6b591ece4
Miguel Fuertes <hkfuertes@gmail.com>
2024-02-21 14:35:36 +0100

    changes

--- uxplay/board/raspberrypi/post-build.sh
+++ uxplay/board/raspberrypi/post-build.sh
@@ -6,7 +6,8 @@ set -e
 # Change this for auto start!
 export CONSOLE=true
 
-if [[ $CONSOLE ]]; then
+if ${CONSOLE:-true} -eq true
+then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
