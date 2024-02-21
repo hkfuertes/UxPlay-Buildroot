36e92f1fd969770e3f29b5ba1031f076ecc4daa1
Miguel Fuertes <hkfuertes@gmail.com>
2024-02-21 10:14:17 +0100

    chagnes

---
+++ uxplay/board/raspberrypi/post-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -u
+set -e
+
+if [ ${CONSOLE:-true}]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+else
+    sed -i '31,34d' ${TARGET_DIR}/etc/inittab
+    echo >> ${TARGET_DIR}/etc/inittab
+    echo "::respawn:/usr/bin/uxplay -nh -n PROJECTOR" >> ${TARGET_DIR}/etc/inittab
+fi
